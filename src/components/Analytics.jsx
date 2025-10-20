e0ebfae8ecfbbcd8f8a4ce349571bbdeb9ea5628
Zen-Coderon <gaganreddyedu@gmail.com>
2025-10-20 18:51:26 +0530

    feat: add AWS outage blog post and GA4 tracking

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.search,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+  }, [location]);
+
+  return null;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import BlogList from './pages/BlogList.jsx';
 import BlogPost from './pages/BlogPost.jsx';
+import { Analytics } from './components/Analytics.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<BlogList />} />
